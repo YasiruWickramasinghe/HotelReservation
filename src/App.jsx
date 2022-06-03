8252b0db6a09d368f7b995651dc56f5577b7281c
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-06-03 18:49:07 +0530

    Hotel reservation app initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome hotel reservation app...!</header>
+    </div>
+  );
+}
+
+export default App;
